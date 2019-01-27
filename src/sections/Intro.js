8b27f1d28f1a41f970ab5820fd716cd1e2a8e61a
Ryan Santos <ryansantos86@gmail.com>
2019-01-27 11:02:08 -0800

    letter spacing

--- src/sections/Intro.js
+++ src/sections/Intro.js
@@ -12,14 +12,27 @@ import {
 import { H1, H2, P } from './../components/text';
 
 class Intro extends Component {
+  handleCanvasRef = canvas => {
+    console.log(canvas.height);
+    const c = canvas.getContext('2d');
+    // rectangle
+    c.fillStyle = 'rgba(0, 0, 0, 0.1)';
+    c.fillRect(0, 50, 50, 50);
+    c.fillRect(100, 50, 50, 50);
+    c.fillRect(200, 50, 50, 50);
+    // c.strokeStyle = '#fa34a3'
+    // c.begin
+  };
+
   render = () => {
     return (
       <IntroSection id={this.props.id}>
         <IntroContent>
+          <Canvas ref={this.handleCanvasRef} />
           <TextContainer>
             <LargeText data-aos="fade-down">HELLO</LargeText>
             <MediumText data-aos="fade-left">I'M RYAN</MediumText>
-            <NormalText data-aos="fade-right">I'M A WEB DEVELOPER</NormalText>
+            <NormalText data-aos="fade-right">CODE + DESIGN</NormalText>
           </TextContainer>
         </IntroContent>
         <ScrollFooter>
@@ -47,6 +60,12 @@ const IntroContent = styled(SectionContent)`
   ${flexCenter};
 `;
 
+const Canvas = styled.canvas`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+`;
+
 const TextContainer = styled.div`
   color: ${({ theme }) => theme.light};
 `;
@@ -69,11 +88,11 @@ const MediumText = styled(H2)`
 `;
 
 const NormalText = styled(P)`
-  letter-spacing: 0.27em;
+  letter-spacing: 0.699em;
   margin-top: 6%;
   @media only screen and (min-width: 425px) {
     font-size: 1.5em;
-    letter-spacing: 0.32em;
+    letter-spacing: 0.78em;
     padding-left: 1%;
   }
 `;
