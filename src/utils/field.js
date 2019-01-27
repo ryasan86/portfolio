aad36b49779aa3a4728055a9ba93a720dcf90cfa
Ryan Santos <ryansantos86@gmail.com>
2019-01-26 20:42:51 -0800

    contact field bg color

--- src/utils/field.js
+++ src/utils/field.js
@@ -2,7 +2,7 @@ import { css } from 'styled-components';
 import { lighten } from 'polished';
 
 const field = css`
-  background: ${({ theme }) => lighten(0.15, theme.light)};
+  background: ${({ theme }) => theme.light};
   padding: 2%;
   border: none;
   outline: none;
